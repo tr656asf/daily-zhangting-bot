a9bde01e44a5a32fca56340c001e413aa1521c98
tr656asf <dyhwangzhe@gmail.com>
2026-03-08 16:20:29 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
+# 读取 GitHub Secrets 或本地环境变量
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+# 测试打印，确保能读取到
+print("TOKEN:", TELEGRAM_TOKEN)
+print("CHAT_ID:", TELEGRAM_CHAT_ID)
