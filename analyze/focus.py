13f9754af58e746cdc89fad52ef24df847b8167e
tr656asf <dyhwangzhe@gmail.com>
2026-03-08 17:21:16 +0800

    Update focus.py

--- analyze/focus.py
+++ analyze/focus.py
@@ -1,9 +1,3 @@
 def get_focus_stocks(stocks):
-
-    stocks = sorted(
-        stocks,
-        key=lambda x: x.get("days", 1),
-        reverse=True
-    )
-
+    stocks = sorted(stocks, key=lambda x: x.get("days", 1), reverse=True)
     return stocks[:3]
