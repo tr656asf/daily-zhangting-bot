0e31ac93907e0250f9b4bdbcf9a670dacaf45c2a
tr656asf <dyhwangzhe@gmail.com>
2026-03-08 15:38:51 +0800

    Add fetch_fail_pool function to retrieve stock data

---
+++ fetch/limit_up_fail.py
@@ -0,0 +1,31 @@
+import requests
+import datetime
+
+
+def get_today():
+    return datetime.datetime.now().strftime("%Y%m%d")
+
+
+def fetch_fail_pool(date=None):
+
+    if date is None:
+        date = get_today()
+
+    url = "https://push2ex.eastmoney.com/getTopicZTPool"
+
+    params = {
+        "ut": "7eea3edcaed734bea9cbfc24409ed989",
+        "dpt": "wz.ztzt",
+        "Pageindex": "1",
+        "pagesize": "200",
+        "sort": "fbt:desc",
+        "date": date
+    }
+
+    r = requests.get(url, params=params)
+
+    data = r.json()
+
+    stocks = data["data"]["pool"]
+
+    return stocks
